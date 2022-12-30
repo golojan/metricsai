900f087b672c326075ec484968cf76d46752f102
Agu Chux <agu.chux@yahoo.com>
2022-12-30 14:10:34 +0100

    Done 7

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <meta charSet="UTF-8" />
       <base href="https://www.metrics.ng/" />
+      <meta charSet="UTF-8" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1"
@@ -20,82 +20,82 @@ export default function Document() {
       />
       <meta
         name="msapplication-TileImage"
-        content="icons/ms-icon-144x144.png"
+        content="/icons/ms-icon-144x144.png"
       />
-      <meta name="msapplication-config" content="icons/browserconfig.xml" />
+      <meta name="msapplication-config" content="/icons/browserconfig.xml" />
       <meta name="description" content="" />
       <meta name="keywords" content="" />
       <meta name="author" content="De-Golojan Technologies Ltd." />
       <link
         rel="apple-touch-icon"
         sizes="57x57"
-        href="icons/apple-icon-57x57.png"
+        href="/icons/apple-icon-57x57.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="60x60"
-        href="icons/apple-icon-60x60.png"
+        href="/icons/apple-icon-60x60.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="72x72"
-        href="icons/apple-icon-72x72.png"
+        href="/icons/apple-icon-72x72.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="76x76"
-        href="icons/apple-icon-76x76.png"
+        href="/icons/apple-icon-76x76.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="114x114"
-        href="icons/apple-icon-114x114.png"
+        href="/icons/apple-icon-114x114.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="120x120"
-        href="icons/apple-icon-120x120.png"
+        href="/icons/apple-icon-120x120.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="144x144"
-        href="icons/apple-icon-144x144.png"
+        href="/icons/apple-icon-144x144.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="152x152"
-        href="icons/apple-icon-152x152.png"
+        href="/icons/apple-icon-152x152.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="icons/apple-icon-180x180.png"
+        href="/icons/apple-icon-180x180.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="icons/android-icon-192x192.png"
+        href="/icons/android-icon-192x192.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="icons/favicon-32x32.png"
+        href="/icons/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="icons/favicon-96x96.png"
+        href="/icons/favicon-96x96.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="icons/favicon-16x16.png"
+        href="/icons/favicon-16x16.png"
       />
-      <link rel="manifest" href="icons/manifest.json" crossOrigin="" />
+      <link rel="manifest" href="/icons/manifest.json" crossOrigin="" />
       <body className="bg-light">
         <Main />
         <NextScript />
