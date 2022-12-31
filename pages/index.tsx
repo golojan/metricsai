69c98a427b92e4a37f3203dacc4d631262c93fbd
Agu Chux <agu.chux@yahoo.com>
2022-12-31 18:31:46 +0100

    Done final 5

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import AllPostFeeds from "./../components/AllPostFeeds";
 import SearchBarLarge from "./../components/SearchBarLarge";
 import Layout from "../components/Layout";
 import { hasAuth } from "../hocs/auth/withAuth";
+import StatusTextBox from "../components/StatusTextBox";
 const cookie = require("js-cookie");
 
 const Home: NextPage = () => {
@@ -14,8 +15,8 @@ const Home: NextPage = () => {
       <main className="col col-xl-6 order-xl-2 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
         <div className="main-content">
           <div className="tab-content" id="pills-tabContent">
-            {isAuth ? null : <SearchBarLarge />}
-            <div className="pt-4 feeds">
+            {isAuth ? <StatusTextBox /> : <SearchBarLarge />}
+            <div className="feeds">
               <AllPostFeeds />
             </div>
           </div>
