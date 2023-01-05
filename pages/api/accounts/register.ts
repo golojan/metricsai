21b232896d020fe9e72076aac0ea8d03e9231f6f
Agu Chux <agu.chux@yahoo.com>
2023-01-05 19:41:34 +0100

    Profile updated

--- pages/api/accounts/register.ts
+++ pages/api/accounts/register.ts
@@ -18,6 +18,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         email,
         gender,
         password,
+        schoolId,
       } = req.body;
       const { Accounts } = await dbCon();
 
@@ -35,6 +36,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         gender: gender,
         passwordKey: password,
         password: hashedPassword,
+        schoolId: schoolId,
       }).catch(catcher);
 
       if (created) {
