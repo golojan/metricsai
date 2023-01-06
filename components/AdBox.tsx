1039e51aa9388f01863a1d93bbe0ac0b5d6a4e9f
Agu Chux <agu.chux@yahoo.com>
2023-01-06 12:12:32 +0100

    Fixed all

--- components/AdBox.tsx
+++ components/AdBox.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 const AdBox = () => {
   return (
     <>
-      <div className="input-group mb-3 shadow-sm rounded-4 min-h-[50px] overflow-hidden py-2 bg-gradient-to-b from-[#556c91] to-[#3266af]">
-        <div className="text-white text-center w-full min-h-[50px]">
-          <h3 className="text-4xl my-3">Ad</h3>
-          <p className="text-sm">Ad space available</p>
-        </div>
+      <div className="input-group my-3 shadow-sm rounded-4 min-h-[50px] overflow-hidden bg-gradient-to-b from-[#556c91] to-[#3266af]">
+        <Link href={"#"}>
+          <img src="/assets/NUCAdvert.png" className="w-full" alt="Metrics" />
+        </Link>
       </div>
     </>
   );
